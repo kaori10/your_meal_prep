8190cfbb1ec830af53eef1ef64e31eeed0c30343
vagrant <vagrant@localhost.localdomain>
2020-09-29 15:42:49 +0000

    [add]devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :admins, controllers: {
+  	sessions: 'admin/admins/sessions',
+  	registrations: 'admin/admins/registrations',
+  }
+
+  devise_for :users, controllers: {
+    sessions: 'public/users/sessions',
+    registrations: 'public/users/registrations',
+  }
+
+
 end
