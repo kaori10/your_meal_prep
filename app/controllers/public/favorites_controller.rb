554e7b6d385e8e3d4725e9e5a9a7187290a8b6d6
vagrant <vagrant@localhost.localdomain>
2020-10-03 06:49:58 +0000

    [add]favorites view

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,4 +1,5 @@
 class Public::FavoritesController < ApplicationController
+
   def create
     @favorite = current_user.favorites.create(recipe_id: params[:recipe_id])
     redirect_back(fallback_location: root_path)
@@ -9,5 +10,10 @@ class Public::FavoritesController < ApplicationController
     @favorite = current_user.favorites.find_by(recipe_id: @recipe.id)
     @favorite.destroy
     redirect_back(fallback_location: root_path)
+  end
+
+  private
+  def set_recipe
+    @recipe = Recipe.find(params[:recipe_id])
   end
 end
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,6 +1,11 @@
 class Public::UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
+    @recipes =@user.recipes
+    favorites = Favorite.where(user_id: current_user.id).pluck(:recipe_id)
+    # ログイン中のユーザーのお気に入りのpost_idカラムを取得
+    @favorite_list = Recipe.find(favorites)
+    # recipesテーブルから、お気に入り登録済みのレコードを取得
   end
 
   def edit
