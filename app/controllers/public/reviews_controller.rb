21a8765d10721cfffd2a52d781bb125ba423f420
vagrant <vagrant@localhost.localdomain>
2020-10-04 04:32:29 +0000

    [add]layout user show

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -1,4 +1,25 @@
 class Public::ReviewsController < ApplicationController
+
   def index
+  	@recipe = Recipe.find(params[:recipe_id])
+  	@reviews = @recipe.reviews
+
   end
+
+  def create
+  	@review = Review.new(review_params)
+  	@review.user_id = current_user.id
+  	if @review.save
+  		redirect_to recipe_reviews_path(@review.recipe)
+  	else
+  		@recipe = Recipe.find(params[:id])
+  		render "public/recipes/show"
+  	end
+  end
+
+  private
+  def review_params
+  	params.require(:review).permit(:recipe_id, :score, :content)
+  end
+
 end
