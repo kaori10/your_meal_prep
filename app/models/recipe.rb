db5a1415ea995c5dd7b70a589960c948fad7a1bb
vagrant <vagrant@localhost.localdomain>
2020-10-04 08:14:08 +0000

    [add]review index

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,19 @@ class Recipe < ApplicationRecord
 	has_many :favorites, dependent: :destroy
 	has_many :reviews, dependent: :destroy
 	attachment :image
+
+	def avg_score
+		unless self.reviews.empty?
+			reviews.average(:score).round(1).to_f
+		else
+			0.0
+		end
+	end
+	def review_score_percentage
+		unless self.reviews.empty?
+			reviews.average(:score).round(1).to_f*100/5
+		else
+			0.0
+		end
+	end
 end
