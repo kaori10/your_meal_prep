da1bcb3eed8283f4953fa866a55ce17881e61111
vagrant <vagrant@localhost.localdomain>
2020-10-05 17:08:03 +0000

    [add]layout

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
       resource :favorites, only: [:create, :destroy]
       resources :reviews, only: [:index, :create]
     end
+    resources :genres, only: [:index]
 
   end
 
